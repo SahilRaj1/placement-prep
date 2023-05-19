99300ccd44b9ff9d149ed801372681bbe5b5fe4a
SahilRaj1 <raj24sahil@gmail.com>
2023-05-19 22:03:20 +0530

    euler theorem

---
+++ dsa/number theory/euler_totient.cpp
@@ -0,0 +1,21 @@
+/* TOPIC: Large Exponentiation with ETF & Euler's Theorem (HARD) */
+
+// TIME COMPLEXITY: O(n log log n)
+// SPACE COMPLEXITY: O(n)
+
+// REFERENCE: https://youtu.be/GApcdkIe9WM
+
+// Euler's totient function - counts the number of integers between 1 and n inclusive, which are coprime to n.
+// phi(n) = n * multiplication of all(1 - 1/p) ; where p = every prime factor of n in the range 1-n.
+// Euler's theorem - (a^b) % m == (a^(b % phi(m))) % m
+// if n is prime => phi(n) = n-1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // no code :)
+
+    return 0;
+}
