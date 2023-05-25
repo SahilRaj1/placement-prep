71d85b4c149eeda023d09b0c3f7e0165ac6eed17
SahilRaj1 <raj24sahil@gmail.com>
2023-05-26 05:16:26 +0530

    stack implementation done

---
+++ dsa/stack and queue/stack/linked_list_stack.cpp
@@ -0,0 +1,118 @@
+/* TOPIC: Stack Implementation using Linked List */
+
+// REFERENCE: None
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    ~Node() {
+        int value = this->data;
+        if (this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+        cout<<"memory is free for node with value "<<value<<endl;
+    }
+    
+};
+
+class Stack {
+
+    private:
+        int ct;
+
+    public:
+        Node* head;
+        Node* tail;
+        int size;
+
+    Stack(int size) {
+        this->head = NULL;
+        this->tail = NULL;
+        this->size = size;
+        this->ct = 0;
+    }
+
+    void push(int val) {
+        if (this->ct == this->size) {
+            cout<<"stack overflow"<<endl;
+            return;
+        }
+        Node* temp = new Node(val);
+        if (ct == 0) {
+            this->head = temp;
+            this->tail = temp;
+            this->ct++;
+            return;
+        }
+        this->tail->next = temp;
+        this->tail = temp;
+        this->ct++;
+        return;
+    }
+
+    void pop() {
+        if (this->ct == 0) {
+            cout<<"stack is empty"<<endl;
+            return;
+        }
+        if (ct == 1) {
+            this->head = NULL;
+            this->tail = NULL;
+            this->ct = 0;
+            return;
+        }
+        Node* temp = this->head;
+        while(temp->next != this->tail) {
+            temp = temp->next;
+        }
+        temp->next = NULL;
+        this->tail = temp;
+        this->ct--;
+        return;
+    }
+
+    int peek() {
+        if (this->ct == 0) {
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+        return this->tail->data;
+    }
+
+    bool isEmpty() {
+        return this->ct==0;
+    }
+
+};
+
+int main() {
+
+    Stack st(5);
+
+    // st.push(1);
+    // st.push(2);
+    // st.push(3);
+    // st.push(4);
+    // st.push(5);
+    // st.push(6);
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    cout << st.isEmpty() << endl;
+
+    return 0;
+}
---
+++ dsa/stack and queue/stack/stack.cpp
@@ -0,0 +1,83 @@
+/* TOPIC: Stack Implementation */
+
+// REFERENCE: https://youtu.be/_6COl6V6mng
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack {
+
+    public:
+        int *arr;
+        int top;
+        int size;
+
+    // constructor
+    Stack(int size) {
+        this->size = size;
+        this->arr = new int[size];
+        this->top = -1;
+    }
+
+    // TIME COMPLEXITY: O(1)
+    void push(int val) {
+        if (this->top == this->size-1) {
+            cout<<"stack overflow"<<endl;
+            return;
+        }
+        this->top++;
+        this->arr[top] = val;
+        return;
+    }
+
+    // TIME COMPLEXITY: O(1)
+    void pop() {
+        if (this->top == -1) {
+            cout<<"stack is empty"<<endl;
+            return;
+        }
+        int temp = this->arr[this->top];
+        this->top--;
+        return;
+    }
+
+    // TIME COMPLEXITY: O(1)
+    int peek() {
+        if (this->top == -1) {
+            cout<<"stack is empty"<<endl;
+            return -1;
+        }
+        int temp = this->arr[this->top];
+        return temp;
+    }
+
+    // TIME COMPLEXITY: O(1)
+    bool isEmpty() {
+        if (this->top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+};
+
+int main() {
+
+    Stack st(5);
+
+    // st.push(1);
+    // st.push(2);
+    // st.push(3);
+    // st.push(4);
+    // st.push(5);
+    // st.push(6);
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    cout << st.isEmpty() << endl;
+
+    return 0;
+}
